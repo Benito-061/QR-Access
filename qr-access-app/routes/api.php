65082977897ea6188af4928d290750d7af8a9201
Benito <benituwase19@gmail.com>
2026-09-05 01:20:45 +0200

    Raccordement des API rapport et corbeille

--- qr-access-app/routes/api.php
+++ qr-access-app/routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\DataSyncController;
+use App\Http\Controllers\Api\ToolsApiController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['web', 'auth'])->group(function () {
     Route::get('/sync', [DataSyncController::class, 'show']);
     Route::post('/sync', [DataSyncController::class, 'store']);
+    Route::get('/downloads', [ToolsApiController::class, 'downloads']);
+    Route::post('/downloads', [ToolsApiController::class, 'recordDownload']);
+    Route::get('/trash', [ToolsApiController::class, 'trash']);
+    Route::post('/trash/{type}/{id}/restore', [ToolsApiController::class, 'restore']);
+    Route::delete('/trash/{type}/{id}', [ToolsApiController::class, 'destroyTrash']);
 });
